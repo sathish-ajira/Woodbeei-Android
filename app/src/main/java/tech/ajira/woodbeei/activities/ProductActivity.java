8f95cda100b5d28211f846008c49c6e6ef748aca
sathish-ajira <sathish@ajirasoft.com>
2017-09-15 11:52:13 +0530

    Sathish |#0| Add custom fonts

---
+++ app/src/main/java/tech/ajira/woodbeei/activities/ProductActivity.java
@@ -0,0 +1,15 @@
+package tech.ajira.woodbeei.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import tech.ajira.woodbeei.R;
+
+public class ProductActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product);
+    }
+}
